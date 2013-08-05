6834902845708dc36150841ea89c2692ab4b493b
jiuyuehe <xm.xiaom@gmail.com>
2013-08-05 22:09:11 +0800

    v0.5
    1个choose 函数

---
+++ public/javascripts/up.js
@@ -0,0 +1,29 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: jiuyuehe
+ * Date: 13-8-5
+ * Time: 下午9:46
+ * To change this template use File | Settings | File Templates.
+ */
+
+var weap={};
+
+
+
+function choose(){
+    var ico = $("img")[1].src;
+
+    alert(ico);
+
+    var img = "<img src=\""+ico+"\">";
+
+    alert(img);
+
+    $("#show").append(img);
+
+}
+
+
+function up (){
+
+}
