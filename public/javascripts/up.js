3f9937c30cc2b570f838132a66255fdbdc487ecf
jiuyuehe <xm.xiaom@gmail.com>
2013-08-09 17:10:39 +0800

    0.6
    0.6

--- public/javascripts/up.js
+++ public/javascripts/up.js
@@ -25,5 +25,31 @@ function choose(){
 
 
 function up (){
+  /*  var data =[1,2,3,4,5,6];
+
+    var sum = 0;
+    data.forEach(function(value){ sum +=value});
+
+    alert(sum);
+
+     data.forEach(function(v,i,a){a[i] = v+1});
+
+    alert(data);*/
+
+    a = [1,2,3,5,7,9,12];
+    b = a.map(function(x){return x*x;});
+    alert(b)
+
+    var  sun = a.reduce(function(x,y){return x+y},0);
+         alert(sun);
+    var product = a.reduce(function(x,y){return x*y},1);
+         alert(product);
+
+
+//    small = a.filter(function(x){return x<7;});
+//    console.log(small);
+//
+//    big = a.filter(function(x,i){return i%2==0;});
+//    console.log(big);
 
 }
